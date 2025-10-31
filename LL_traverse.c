46ad74b3d71762de96613b58076c9f0bce6aafa9
imran <mdimranhasan2023@gmail.com>
2025-10-31 12:01:05 +0600

    Traverse complete in link list

--- LL_traverse.c
+++ LL_traverse.c
@@ -1,11 +1,30 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<math.h>
+
 
 struct Node{
 	int data ;
 	struct Node *next;
 };
-
+void Maximum(struct Node *head){
+	struct Node *ptr=head;
+	int max=0;
+	while(ptr!=NULL){
+		if(ptr->data>max) max=ptr->data;
+		ptr=ptr->next;
+	}
+	printf("\nMaximum value is :  %d",max);
+}
+void Minimum(struct Node *head){
+	struct Node *ptr=head;
+	int min=10000;
+	while(ptr!=NULL){
+		if(ptr->data<min) min=ptr->data;
+		ptr=ptr->next;
+	}
+	printf("\nMaximum value is :  %d",min);
+}
 void Traverse(struct Node *head){
 	printf("Traverse LinkList: \n");
 	struct Node *ptr =head ;
@@ -18,6 +37,34 @@ void Traverse(struct Node *head){
 			ptr=ptr->next;
     	}
 	}
+}
+void Total_value(struct Node *head){
+	struct Node *ptr=head;
+	int sum=0;
+	while(ptr!=NULL){
+		sum+=ptr->data;
+		ptr=ptr->next;
+
+	}
+	printf("\nThe total sum is :  %d",sum);
+}
+void Sin_value(struct Node *head){
+	struct Node *ptr = head ;
+	while(ptr!=NULL){
+		printf("\nThe sin value of %d is : %f",ptr->data,(3.1416/180)*(ptr->data) );
+		ptr = ptr->next;
+	}
+}
+void Average_Value(struct Node *head){
+	struct Node *ptr = head;
+	int sum=0,cnt=0;
+	while(ptr!=NULL){
+		sum+=ptr->data;
+		ptr=ptr->next;
+		cnt++;
+    }
+	float average = sum/cnt;
+	printf("\nThe average value is: %f",average);
 }
 
 int main()
@@ -43,6 +90,14 @@ int main()
 		}
 	}
 	Traverse(head);
+	Maximum(head);
+	Minimum(head);
+	Total_value(head);
+	Average_Value(head);
+	Sin_value(head);
+
+
+	
 
 	
 	return 0;
