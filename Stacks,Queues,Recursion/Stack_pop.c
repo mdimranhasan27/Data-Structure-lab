6c68ac64eeba48e84d6fc797e5f875ed0bc8d0d3
imran <mdimranhasan2023@gmail.com>
2026-01-10 11:43:18 +0600

    Stack pop using array

--- Stacks,Queues,Recursion/Stack_pop.c
+++ Stacks,Queues,Recursion/Stack_pop.c
@@ -1,31 +1,4 @@
-#include <stdio.h>
-#define MAX 10
-
-int stack[MAX];
-int top = -1;
-
-void push(int item) {
-    if(top == MAX - 1) return;
-    stack[++top] = item;
-}
-
-int pop() {
-    if(top == -1) {
-        printf("Stack Underflow\n");
-        return -1;
-    }
-    return stack[top--];
-}
-
-int main() {
-    push(5);
-    push(15);
-    push(25);
-
-    printf("Popped element: %d\n", pop());
-    printf("Popped element: %d\n", pop());
-    printf("Popped element: %d\n", pop());
-    printf("Popped element: %d\n", pop());
-
+#include<stdio.h>
+int main(){
     return 0;
 }
